2a4c34e8039d4fcccd1e17ea2d71bb5882ca9cbc
AnaM6 <ana.mendoza.mt@correo.usa.edu.co>
2021-10-25 15:07:54 -0500

    Agregando comentarios para reservation

--- src/main/java/co/usa/ciclo3/ciclo3/service/ReservationService.java
+++ src/main/java/co/usa/ciclo3/ciclo3/service/ReservationService.java
@@ -10,20 +10,41 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+/**
+ * Clase Reservation Service
+ * Realizado por Ana Mendoza grupo G1
+ */
 @Service
 public class ReservationService {
 
     @Autowired
+    /**
+     * Definición de variable reservationRepository
+     */
     private ReservationRepository reservationRepository;
 
+    /**
+     * Función para obtener los elementos de la tabla reservation
+     * @return todos los elementos
+     */
     public List<Reservation> getAll(){
         return reservationRepository.getAll();
     }
 
+    /**
+     * Función para obtener una reserva en especifico
+     * @param idReservation variable para indicar cual deseo obtener
+     * @return el elemento que pertenece a ese id
+     */
     public Optional<Reservation> getReservation(int idReservation){
         return reservationRepository.getReservation(idReservation);
     }
 
+    /**
+     * Función para guardar elementos
+     * @param r elemento a guardar
+     * @return elemento guardado
+     */
     public Reservation save(Reservation r){
         if(r.getIdReservation()==null){
             return reservationRepository.save(r);
@@ -37,6 +58,11 @@ public class ReservationService {
         }
     }
 
+    /**
+     * Función para actualizar un elemento que ya exista
+     * @param reservation elemento a actualizar
+     * @return el elemento actualizado
+     */
     public Reservation update(Reservation reservation){
         if(reservation.getIdReservation()!=null){
             Optional<Reservation>g= reservationRepository.getReservation(reservation.getIdReservation());
@@ -52,6 +78,12 @@ public class ReservationService {
         }
         return reservation;
     }
+
+    /**
+     * Función para borrar un elemento
+     * @param id indica el id del elemento que se va a borrar
+     * @return lugar vacio del elemento que fue borrado
+     */
 
     public boolean deleteReservation(int id){
         Boolean del = getReservation(id).map(reservation -> {
